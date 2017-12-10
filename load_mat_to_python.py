ffb854238bb7200153aedcde562ddb44ae1d9679
Kashish Gupta <kashishg@wharton.upenn.edu>
2017-12-10 18:16:42 -0500

    Created script to load matlab array to python

---
+++ load_mat_to_python.py
@@ -0,0 +1,12 @@
+import scipy.io as sio
+import numpy as np
+
+sio.whosmat('filename.mat')
+
+#matlab_contents = sio.loadmat('filename.mat')
+
+#target_array = matlab_contents['array_name_here']
+
+#print target_array
+
+#np.save('nyu_dataset', target_array);
