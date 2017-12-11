3816a21ec18c94ab46381b80531b7f5753473680
Kashish Gupta <kashishg@wharton.upenn.edu>
2017-12-10 19:33:46 -0500

    Updated bounding boxes

--- find_BB_and_depth.py
+++ find_BB_and_depth.py
@@ -8,26 +8,19 @@ def (img_d):
 	ret, threshed_img = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 127, 255, cv2.THRESH_BINARY)
 	image, contours, hier = cv2.findContours(threshed_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 	
-	for c in contours:
+	output = np.zeros([len(contours),5])
+	
+	for i in range(len(contours)):
 		# get the bounding rect
-		x, y, w, h = cv2.boundingRect(c)
+		x, y, w, h = cv2.boundingRect(contours[i])
 		
 		# draw a green rectangle to visualize the bounding rect
 		cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-		# get the min area rect
-		rect = cv2.minAreaRect(c)
-		box = cv2.boxPoints(rect)
-		# convert all coordinates floating point values to int
-		box = np.int0(box)
-		# draw a red 'nghien' rectangle
-		cv2.drawContours(img, [box], 0, (0, 0, 255))
-		
-	print(len(contours))
-	cv2.drawContours(img, contours, -1, (255, 255, 0), 1)
-	 
-	cv2.imshow("contours", img)
+		output[i,:] = [x,y,w,h,depth]
 	
+	#cv2.drawContours(img, contours, -1, (255, 255, 0), 1)
+	#cv2.imshow("contours", img)
+	cv2.destroyAllWindows()
 	
 	output = x, y, p_width, p_height, depth
 	return output
