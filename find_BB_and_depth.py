314d321e246ac6f95f45d2378ecc6e6034e7a901
Kashish Gupta <kashishg@wharton.upenn.edu>
2017-12-10 19:41:58 -0500

    Bounding boxes work in theory

--- find_BB_and_depth.py
+++ find_BB_and_depth.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cv2
 
-def (img_d):
-	img_no_d = img_d[:,:,:,]
-	img = cv2.pyrDown(img_no_d)
+#takes in a rgbd 4 dimensional image
+def (img_rgb, pixel_depths):
+	img = cv2.pyrDown(img_rgb)
 	
 	ret, threshed_img = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 127, 255, cv2.THRESH_BINARY)
 	image, contours, hier = cv2.findContours(threshed_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -16,11 +16,13 @@ def (img_d):
 		
 		# draw a green rectangle to visualize the bounding rect
 		cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+		
+		depth = np.mean(pixel_depths[y:y+h, x:x+w])
+		
 		output[i,:] = [x,y,w,h,depth]
 	
 	#cv2.drawContours(img, contours, -1, (255, 255, 0), 1)
 	#cv2.imshow("contours", img)
 	cv2.destroyAllWindows()
 	
-	output = x, y, p_width, p_height, depth
 	return output
