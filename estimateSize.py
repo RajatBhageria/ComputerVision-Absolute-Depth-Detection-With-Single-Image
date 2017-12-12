08a040acaf54a7acb0715679487e8b1f0cde9dfa
Bill He <billhebillhe@gmail.com>
2017-12-12 12:22:26 -0500

    added back NN work

--- estimateSize.py
+++ estimateSize.py
@@ -138,21 +138,35 @@ def estimateSize():
 
     # Part 8: Fit a Neural nets with training data
 
-    # re-massage the features
-    # width of the bbox, Px, height of bbox, Py, depth
-    heightWidth = np.hstack((train_height[:, [0, 2]], train_width[:, [0, 2]]))
-    Xtrain = np.c_[heightWidth, train_width[:, 1]]
+    #Get X train
+    px = imageLabels[:, 9] / 2
+    depth = imageLabels[:, 8]
+    height = imageLabels[:, 6]
+    py = imageLabels[:, 10] / 2
+    width = imageLabels[:, 7]
 
-    # re-massage the ytrain
-    #heights and widths in meters
-    Ytrain = np.vstack((label_height, label_width)).T
+    Xtrain_height = np.c_[height, px, depth]
+    Xtrain_width = np.c_[width, py, depth]
 
-    # get the Xtest data
-    heightWidth = np.hstack((Xtest_height[:, [0, 2]], Xtest_width[:, [0, 2]]))
-    Xtest = np.c_[heightWidth, train_width[:, 1]]
+    # Get y train
+    #Ytrain = np.vstack((label_height, label_width)).T
+    Ytrain_height = label_height.T
+    Ytrain_width = label_width.T
 
-    y_hat_NN = runNeuralNet(Xtrain, Ytrain, Xtest)
+    #Get x test
+    px_test = imageUnLabeled[:, 9] / 2
+    depth_test = imageUnLabeled[:, 8]
+    height_test = imageUnLabeled[:, 6]
+    py_test = imageUnLabeled[:, 10] / 2
+    width_test = imageUnLabeled[:, 7]
 
+    Xtest_height = np.c_[height_test, px_test, depth_test]
+    Xtest_width = np.c_[width_test, py_test,depth_test]
+
+    y_hat_height = runDNN(Xtrain_height, Ytrain_height, Xtest_height)
+    y_hat_width = runDNN(Xtrain_width, Ytrain_width, Xtest_width)
+    print y_hat_height
+    print y_hat_width
 
 if __name__ == '__main__':
     estimateSize()
