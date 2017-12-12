c4d0e6dbdda70b13fc02c7bff137273af3c9534c
Bill He <billhebillhe@gmail.com>
2017-12-12 11:56:48 -0500

    updated estimateSize

--- estimateSize.py
+++ estimateSize.py
@@ -3,7 +3,7 @@ import cv2
 import csv
 from find_BB_and_depth import find_BB_and_depth
 # import load_mat_to_python
-from linreg import LinearRegression as LinearRegression
+from linreg_closedform import LinearRegressionClosedForm as LinearRegression
 from PIL import Image
 # from NeuralNet import runNeuralNet
 import sys
