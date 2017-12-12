07df681b9e3dae292779336b063d6f92a1296b0d
Bill He <billhebillhe@gmail.com>
2017-12-12 12:26:18 -0500

    added back variable name

---
+++ labeling/BoundingBoxLabeling.py
@@ -0,0 +1,55 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import numpy as np
+import cv2
+
+# takes in a rgbd 4 dimensional image
+def BoundingBoxLabeling(img_rgb, pixel_depths, drawContours = False, imageNum = -1):
+    img = cv2.pyrDown(img_rgb)
+
+    ret, threshed_img = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 127, 255, cv2.THRESH_BINARY)
+    image, contours, hier = cv2.findContours(threshed_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    listOfBBs = []
+
+    for i in range(len(contours)):
+        # get the bounding rect
+        x, y, w, h = cv2.boundingRect(contours[i])
+
+        # draw a green rectangle to visualize the bounding rect
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        depth = np.mean(pixel_depths[2 * y:2 * (y + h), 2 * x:2 * (x + w)])
+        if w > 15 and h > 15 and w < 300 and h < 200:
+            listOfBBs.append(i)
+            output[i, :] = x * 2, y * 2, w * 2, h * 2, depth
+
+    cv2.destroyAllWindows()
+
+    print(listOfBBs)
+
+    for i in listOfBBs:
+        img = cv2.pyrDown(img_rgb)
+
+        ret, threshed_img = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 127, 255, cv2.THRESH_BINARY)
+        image, contours, hier = cv2.findContours(threshed_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        # get the bounding rect
+        x, y, w, h = cv2.boundingRect(contours[i])
+
+        # draw a green rectangle to visualize the bounding rect
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        cv2.imshow("contours", img)
+        cv2.waitKey(10)
+
+        line = raw_input("Current imgnum,bbnum: " + str(imageNum) + "," + str(i) + ",")
+        if len(line) > 0:
+            f = open("../data/currentlabeling.dat","a+")
+            f.write(str(imageNum) + "," + str(i) + "," + line)
+            f.write("\r\n")
+            f.close()
+
+        cv2.destroyAllWindows()
+
+    return []
+    # the type is a [k, 5] array, the 5 features are x, y, w, h, depth_of_bb_in_meters
--- testingRig.py
+++ testingRig.py
@@ -40,22 +40,9 @@ def estimateSize():
         imgNum = int(imageLabels[i, 0])
         imgi = images[:, :, :, imgNum]
         h, w, c = imgi.shape
-
-        # show the image
-        # pilimg = Image.fromarray(imgi, 'RGB')
-        # pilimg.show()
-
-        # bbox size [k,5] where n is image number, k is num of objects in each image
-        # last dimension has x, y, height, width, depth of each bbox in image i
         bbox = find_BB_and_depth(imgi, depths[:, :, imgNum], False)
-
-        # add to the allBBoxes matrix
         k = int(imageLabels[i, 1])
-
-        # add the bbox values to the imageLabel
         imageLabels[i, 4:9] = bbox[k]
-
-        # add the height width of the image to the imageLabels
         imageLabels[i, 9:11] = (h, w)
 
     # Part 4: Aggregate training data
@@ -66,10 +53,6 @@ def estimateSize():
                     imageLabels[:, 6]) * imageLabels[:, 8]
     train_width = (imageLabels[:, 10] / 2 -
                    imageLabels[:, 7]) * imageLabels[:, 8]
-
-                   # Old features
-    # train_height = imageLabels[:, [6, 8, 9]]
-    # train_width = imageLabels[:, [7, 8, 10]]
 
     # Y train- training heights and widths
     label_height = imageLabels[:, 2]
@@ -84,6 +67,14 @@ def estimateSize():
     # array to hold (img#, bb#, null, null, x, y, h, w, d, img_h, img_w)
     imageUnLabeled = np.zeros((n, 11))
     imageUnLabeled[:, 0:2] = unlabeledWithDescription
+
+    # Part 6: Fit a Linear Regression with training data
+    # do training on linear regression
+    linreg_x = LinearRegression(regLambda=1E-8)
+    linreg_y = LinearRegression(regLambda=1E-8)
+
+    linreg_x.fit(train_height, label_height)
+    linreg_y.fit(train_width, label_width)
 
     # Part 6: Create bounding boxes for our testing images
     for i in range(n):
@@ -100,22 +91,18 @@ def estimateSize():
         bbox = find_BB_and_depth(imgi, depths[:, :, imgNum], False)
 
         # get the bouning box number
-        k = int(imageUnLabeled[i, 1])
+        k = int(imageLabels[i, 1])
 
         # add the bbox values to the imageLabel
-        imageUnLabeled[i, 4:9] = bbox[k]
+        imageLabels[i, 4:9] = bbox[k]
 
         # add the height width of the image to the imageLabels
-        imageUnLabeled[i, 9:11] = (h, w)
+        imageLabels[i, 9:11] = (h, w)
 
     Xtest_height = (imageUnLabeled[:, 10] / 2 -
                     imageUnLabeled[:, 7]) * imageUnLabeled[:, 8]
     Xtest_width = (imageUnLabeled[:, 10] / 2 -
                    imageUnLabeled[:, 7]) * imageUnLabeled[:, 8]
-
-    # Old
-    # Xtest_height = imageUnLabeled[:, [6, 8, 9]]
-    # Xtest_width = imageUnLabeled[:, [7, 8, 10]]
 
     # Part 6: Fit a Linear Regression with training data
     # do training on linear regression
@@ -138,35 +125,21 @@ def estimateSize():
 
     # Part 8: Fit a Neural nets with training data
 
-    #Get X train
-    px = imageLabels[:, 9] / 2
-    depth = imageLabels[:, 8]
-    height = imageLabels[:, 6]
-    py = imageLabels[:, 10] / 2
-    width = imageLabels[:, 7]
+    # re-massage the features
+    # width of the bbox, Px, height of bbox, Py, depth
+    heightWidth = np.hstack((train_height[:, [0, 2]], train_width[:, [0, 2]]))
+    Xtrain = np.c_[heightWidth, train_width[:, 1]]
 
-    Xtrain_height = np.c_[height, px, depth]
-    Xtrain_width = np.c_[width, py, depth]
+    # re-massage the ytrain
+    #heights and widths in meters
+    Ytrain = np.vstack((label_height, label_width)).T
 
-    # Get y train
-    #Ytrain = np.vstack((label_height, label_width)).T
-    Ytrain_height = label_height.T
-    Ytrain_width = label_width.T
+    # get the Xtest data
+    heightWidth = np.hstack((Xtest_height[:, [0, 2]], Xtest_width[:, [0, 2]]))
+    Xtest = np.c_[heightWidth, train_width[:, 1]]
 
-    #Get x test
-    px_test = imageUnLabeled[:, 9] / 2
-    depth_test = imageUnLabeled[:, 8]
-    height_test = imageUnLabeled[:, 6]
-    py_test = imageUnLabeled[:, 10] / 2
-    width_test = imageUnLabeled[:, 7]
+    y_hat_NN = runNeuralNet(Xtrain, Ytrain, Xtest)
 
-    Xtest_height = np.c_[height_test, px_test, depth_test]
-    Xtest_width = np.c_[width_test, py_test,depth_test]
-
-    y_hat_height = runDNN(Xtrain_height, Ytrain_height, Xtest_height)
-    y_hat_width = runDNN(Xtrain_width, Ytrain_width, Xtest_width)
-    print y_hat_height
-    print y_hat_width
 
 if __name__ == '__main__':
     estimateSize()
