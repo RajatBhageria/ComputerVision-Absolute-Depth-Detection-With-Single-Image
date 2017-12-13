0adf85aa9e02e1b72e14ff2dc8954a00029534cc
Kashish Gupta <kashishg@wharton.upenn.edu>
2017-12-12 22:37:31 -0500

    Going back to estimateDepth, not estimateDepth2

--- estimateDepth.py
+++ estimateDepth.py
@@ -11,10 +11,11 @@ tf.logging.set_verbosity(tf.logging.INFO)
 
 
 def cnn_model_fn(features, labels, mode):
+  print("printing to mark the start of an iteration")
   """Model function for CNN."""
   # Input Layer
   # Reshape X to 4-D tensor: [batch_size, width, height, channels]
-  # MNIST images are 28x28 pixels, and have one color channel
+  # images are 28x28 pixels, and have one color channel
   input_layer = tf.reshape(features["x"], [-1, 480, 640, 3])
 
   # Convolutional Layer #1
@@ -24,7 +25,7 @@ def cnn_model_fn(features, labels, mode):
   # Output Tensor Shape: [batch_size, 28, 28, 8]
   conv1 = tf.layers.conv2d(
       inputs=input_layer,
-      filters=8,
+      filters=2,
       kernel_size=[5, 5],
       padding="same",
       activation=tf.nn.relu)
@@ -33,7 +34,7 @@ def cnn_model_fn(features, labels, mode):
   # First max pooling layer with a 2x2 filter and stride of 2
   # Input Tensor Shape: [batch_size, 480, 640, 8]
   # Output Tensor Shape: [batch_size, 240, 320, 8]
-  pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+  pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[4, 4], strides=4)
 
   # Convolutional Layer #2
   # Computes 64 features using a 5x5 filter.
@@ -42,7 +43,7 @@ def cnn_model_fn(features, labels, mode):
   # Output Tensor Shape: [batch_size, 240, 320, 16]
   conv2 = tf.layers.conv2d(
       inputs=pool1,
-      filters=16,
+      filters=4,
       kernel_size=[5, 5],
       padding="same",
       activation=tf.nn.relu)
@@ -56,13 +57,13 @@ def cnn_model_fn(features, labels, mode):
   # Flatten tensor into a batch of vectors
   # Input Tensor Shape: [batch_size, 120, 160, 16]
   # Output Tensor Shape: [batch_size, 120 * 160 * 16]
-  pool2_flat = tf.reshape(pool2, [-1, 120 * 160 * 16])
+  pool2_flat = tf.reshape(pool2, [-1, 60 * 80 * 4])
 
   # Dense Layer
   # Densely connected layer with 1024 neurons
   # Input Tensor Shape: [batch_size, 120 * 160 * 16]
   # Output Tensor Shape: [batch_size, 120*160]
-  dense = tf.layers.dense(inputs=pool2_flat, units=19200, activation=tf.nn.relu)
+  dense = tf.layers.dense(inputs=pool2_flat, units=128, activation=tf.nn.relu)
 
   # Add dropout operation; 0.6 probability that element will be kept
   dropout = tf.layers.dropout(
@@ -75,24 +76,25 @@ def cnn_model_fn(features, labels, mode):
 
   predictions = {
       # Generate predictions (for PREDICT and EVAL mode)
-      "classes": tf.argmax(input=logits, axis=1),
+      "classes": logits
+      #"classes": tf.argmax(input=logits, axis=1),
       # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
       # `logging_hook`.
-      "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+      #"probabilities": tf.nn.softmax(logits, name="softmax_tensor")
   }
   if mode == tf.estimator.ModeKeys.PREDICT:
+    print(predictions)
     return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
   # Calculate Loss (for both TRAIN and EVAL modes)
   #onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=10) #honxestly this depth should be 10 but i dont care
-  print('i am printing here')
-  print(tf.cast(labels, tf.int32))
-  loss = tf.losses.softmax_cross_entropy(
-      onehot_labels=onehot_labels, logits=logits)
+  print('i am printing here right before calculating losses')
+  #print(tf.cast(labels, tf.int32))
+  loss = tf.reduce_sum(tf.square(labels - logits))
 
   # Configure the Training Op (for TRAIN mode)
   if mode == tf.estimator.ModeKeys.TRAIN:
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
     train_op = optimizer.minimize(
         loss=loss,
         global_step=tf.train.get_global_step())
@@ -102,47 +104,40 @@ def cnn_model_fn(features, labels, mode):
   eval_metric_ops = {
       "accuracy": tf.metrics.accuracy(
           labels=labels, predictions=predictions["classes"])}
+  print(predictions["classes"])
   return tf.estimator.EstimatorSpec(
       mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
-data = np.load('nyu_dataset_images.npy')
-labels_data = np.load('nyu_dataset_depths.npy')
-train_data = np.array([data[:,:,:,i] for i in range(20)], dtype='float16')
-eval_data = np.array([data[:,:,:,i] for i in range(20,30)], dtype='float16')
-#train_labels = np.array([labels_data[:,:,i] for i in range(20)])
-#eval_labels = np.array([labels_data[:,:,i] for i in range(20,30)])
-train_labels = np.array([np.array(labels_data[:,:,i]).flatten() for i in range(20)]) #use these for flattened labels
-eval_labels = np.array([np.array(labels_data[:,:,i]).flatten() for i in range(20,30)])
-
 def main(unused_argv):
-  # Load training and eval data
-  #mnist = tf.contrib.learn.datasets.load_dataset("mnist")
-  #train_data = mnist.train.images  # Returns np.array
-  #train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
-  #eval_data = mnist.test.images  # Returns np.array
-  #eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+  data = np.load('nyu_dataset_images.npy')
+  labels_data = np.load('nyu_dataset_depths.npy')
+  train_data = np.array([data[:, :, :, i] for i in range(20)], dtype='float32')
+  eval_data = np.array([data[:, :, :, i] for i in range(20, 30)], dtype='float32')
+  # train_labels = np.array([labels_data[:,:,i] for i in range(20)])
+  # eval_labels = np.array([labels_data[:,:,i] for i in range(20,30)])
+  train_labels = np.array([np.array(labels_data[:, :, i]).flatten() for i in range(20)])  # use these for flattened labels
+  eval_labels = np.array([np.array(labels_data[:, :, i]).flatten() for i in range(20, 30)])
 
   # Create the Estimator
-  mnist_classifier = tf.estimator.Estimator(
+  depth_classifier = tf.estimator.Estimator(
       model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model")
 
   # Set up logging for predictions
   # Log the values in the "Softmax" tensor with label "probabilities"
-  tensors_to_log = {"probabilities": "softmax_tensor"}
-  logging_hook = tf.train.LoggingTensorHook(
-      tensors=tensors_to_log, every_n_iter=50)
+  #tensors_to_log = {"probabilities": "softmax_tensor"}
+  #logging_hook = tf.train.LoggingTensorHook(
+      #tensors=tensors_to_log, every_n_iter=50)
 
   # Train the model
   train_input_fn = tf.estimator.inputs.numpy_input_fn(
       x={"x": train_data},
       y=train_labels,
-      batch_size=100,
+      batch_size=1,
       num_epochs=None,
       shuffle=True)
-  mnist_classifier.train(
+  depth_classifier.train(
       input_fn=train_input_fn,
-      steps=20000,
-      hooks=[logging_hook])
+      steps=2)
 
   # Evaluate the model and print results
   eval_input_fn = tf.estimator.inputs.numpy_input_fn(
@@ -150,7 +145,7 @@ def main(unused_argv):
       y=eval_labels,
       num_epochs=1,
       shuffle=False)
-  eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+  eval_results = depth_classifier.evaluate(input_fn=eval_input_fn)
   print(eval_results)
 
 
